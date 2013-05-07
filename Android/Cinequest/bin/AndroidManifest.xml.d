9b7fd53f1c28a732aac5270be784ebc9bd7efd3f
Jeffrey Yi <jeffrey.yi@sjsu.edu>
2013-05-07 09:18:40 -0700

    Removed Android Project

--- Android/Cinequest/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/jeff/PhoneGap/Cinequest/bin/AndroidManifest.xml : \
-/home/jeff/PhoneGap/Cinequest/AndroidManifest.xml \
--- Android/Cinequest/bin/Cinequest-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/jeff/PhoneGap/Cinequest/bin/Cinequest-debug-unaligned.apk : \
-/home/jeff/PhoneGap/Cinequest/bin/Cinequest.ap_ \
-/home/jeff/PhoneGap/Cinequest/bin/classes.dex \
-/home/jeff/PhoneGap/Cinequest/libs/cordova-2.6.0.jar \
